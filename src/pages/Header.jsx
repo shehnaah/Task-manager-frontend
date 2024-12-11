4cec80641087b69c228fa01a2379c33a4ea40921
Hazeeb88 <hazeebzdco@gmail.com>
2024-12-11 10:37:07 +0300

    updated

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import "./Header.css";
-import logo from '../images/z blue logo.jpg';
-const Header = () => {
+import logo from '../images/z blue logo.jpg'; // Ensure proper naming conventions
+
+const Header = ({ userName = "Guest User", profilePicture = "placeholder.jpg" }) => {
   return (
     <header className="header">
       <div className="header-left">
-      <img src={logo} alt="Logo" className="logo" />
+        <img src={logo} alt="Logo" className="logo" />
         <input
           type="text"
           className="search-bar"
           placeholder="Search in services, startups, etc..."
+          aria-label="Search"
         />
       </div>
       <nav className="nav-menu">
-        <a href="/" className="nav-item active">Home</a>
+        <a href="/" className="nav-item active" aria-current="page">Home</a>
         <a href="/blog" className="nav-item">Blog</a>
         <a href="/club" className="nav-item">Club</a>
-        <a href="/about-us" className="nav-item">About us</a>
+        <a href="/about-us" className="nav-item">About Us</a>
       </nav>
       <div className="header-right">
-        <span className="user-name">Kanee Thora</span>
+        <span className="user-name">{userName}</span>
         <img
-          src="/path-to-profile-picture.jpg"
-          alt="Profile"
+          src={profilePicture}
+          alt={`${userName}'s Profile`}
           className="profile-picture"
         />
       </div>
