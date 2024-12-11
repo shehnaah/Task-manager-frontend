0e588626706489707b33dd4853ab5211d498c0ce
shehnaah <shehna0501@gmail.com>
2024-12-11 18:24:21 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
-import Home from '../src/components/Home'
+import Main from '../src/pages/Main'
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      
+      <Route path='/' element={<Main/>}/>
     </Routes>
     
 
